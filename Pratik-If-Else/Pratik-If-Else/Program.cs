4dc8f43e935f678e976926209f8c3dfccb7c1c9c
OrkunDemir5 <orkun.d1999@gmail.com>
2024-10-02 11:01:03 +0300

    Pratik-if-else

---
+++ Pratik-If-Else/Pratik-If-Else/Program.cs
@@ -0,0 +1,44 @@
+﻿int result; //Dönüşüm başarısız olduğunda varsayılan değer
+
+Console.WriteLine("Lütfen bir sayi giriniz");
+string numb = Console.ReadLine();
+
+//int.TryParse(numb, out result);
+
+//sayının tek mi çift mi olduğunu ve int'e dönüşüyor mu kontrol et
+if (int.TryParse(numb, out result) && result % 2 == 0)
+{
+    if (result == 10)
+    {
+        Console.WriteLine("Sayi 10'a eşittir");
+    }
+    else if (result < 10)
+    {
+        Console.WriteLine("Sayi 10'dan küçüktür");
+    }
+    else if (result > 10)
+    {
+        Console.WriteLine("Sayi 10'dan büyüktür");
+    }
+    Console.WriteLine("Sayi çifttir");
+}else if (int.TryParse(numb, out result) && result % 2 != 0)
+{
+    if (result == 10)
+    {
+        Console.WriteLine("Sayi 10'a eşittir");
+    }
+    else if (result < 10)
+    {
+        Console.WriteLine("Sayi 10'dan küçüktür");
+    }
+    else if (result > 10)
+    {
+        Console.WriteLine("Sayi 10'dan büyüktür");
+    }
+    Console.WriteLine("Sayi tektir");
+}
+else
+{
+    Console.WriteLine("Hatalı sayı girdiniz");
+}
+
